bf50be66dcf2d69f3dbde9a8fbd469e71e7af24a
Geeta Ingle <42793643+geetaingle@users.noreply.github.com>
2021-02-06 01:43:57 +0530

    Update 2. Reading video.py

--- Read images, videos and webcam/2. Reading video.py
+++ Read images, videos and webcam/2. Reading video.py
@@ -1,10 +1,10 @@
 import cv2
 
-cap = cv2.VideoCapture("video.mp4")
+cap = cv2.VideoCapture("video.mp4") 
 
 while True:
-    success, img = cap.read()
-    cv2.imshow("Video",img)
-    if cv2.waitKey(1) & 0xFF ==ord('q'):
+    success, img = cap.read() #if cap.read() frame returns success = "True"
+    cv2.imshow("Video",img) #Shows frames read by img
+    if cv2.waitKey(1) & 0xFF ==ord('q'): #video will stop on pressing "q"
         break
 cv2.destroyAllWindows()
